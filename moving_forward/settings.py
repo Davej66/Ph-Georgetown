4dd19715676ab52c80bde58d612ba2dc17975635
Davej66 <daijacobs@gmail.com>
2023-08-30 14:00:35 +0000

    Updating csrf trusted origins

--- moving_forward/settings.py
+++ moving_forward/settings.py
@@ -50,8 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 
-    # 'django.contrib.sites',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
